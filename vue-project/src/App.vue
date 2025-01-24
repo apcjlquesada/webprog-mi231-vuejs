20668476681477a4f1130becb24bb201d78d5c02
Joe Gene Quesada <52223592+apcjlquesada@users.noreply.github.com>
2025-01-24 09:16:46 +0800

    updated position

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <h1>Food</h1>
+  <personal-profile/> <br>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
-  <personal-profile/> <br>
+  
 </template>
 
 <script></script>
